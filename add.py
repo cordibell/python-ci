e89aa52314dca6ad8a1dd4a75239cfd725d14b18
Bethan Thurgood <bethan@thurgood.org.uk>
2025-11-24 09:48:42 +0000

    fixed indentation

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 def add(x: int, y: int) -> int:
     """Returns the sum of two integers."""
-     if not type(x) == int or not type(y) == int:
+    if not type(x) == int or not type(y) == int:
          raise TypeError("Both arguments must be integers.")
     return x + y
 
