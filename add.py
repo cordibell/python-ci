0e5555cdee5a165e72466ef6c8e6f13364f9962b
Bethan Thurgood <bethan@thurgood.org.uk>
2025-11-24 09:36:21 +0000

    fixed argument check to function add

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 def add(x: int, y: int) -> int:
     """Returns the sum of two integers."""
-    if not type(x) == int or not type(y) == int:
-        raise TypeError("Both arguments must be integers.")
+    # if not type(x) == int or not type(y) == int:
+    #     raise TypeError("Both arguments must be integers.")
     return x + y
